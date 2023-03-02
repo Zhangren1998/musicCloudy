d72a9186a6c9cea6021ebc9c22a3a8a2259088fd
Asa_zr <qq926197502@gmail.com>
2023-03-02 23:15:31 +0800

    1

---
+++ src/pages/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'VitStudy1Index',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {},
+}
+</script>
+
+<style lang="sass" scoped></style>
